55e6e8ecb054395d0c75ec2c782d3bf5a6f11ccb
chrisgorrod <chrisgorrod@Chriss-MacBook-Pro.local>
2015-09-07 14:03:40 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]> <html class="lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]> <html class="lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+	<meta property="og:type" content="og:website" />
+	<meta property="og:title" content="SkyWatch" />
+	<meta property="og:image" content="#" />
+	<meta property="og:description" content="New Website Description" />
+	<meta property="og:url" content="http://new.start/" />
+
+	<title>SkyWatch</title>
+	<meta name="description" content="New Website Description" />
+
+	<link rel="canonical" href="" />
+	<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+	<link rel="stylesheet" href="/assets/css/main.css" />
+	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+
+<header class="cd-main-header">
+		<a class="cd-logo" href="#0"><img src="/assets/img/logo.png" alt="Logo"></a>
+
+		<ul class="cd-header-buttons">
+			<li><a class="cd-nav-trigger" href="#cd-primary-nav">Menu<span></span></a></li>
+			<!-- <li><a class="cd-search-trigger" href="#cd-search">Search<span></span></a></li> -->
+		</ul> <!-- cd-header-buttons -->
+	</header>
+
+
+	<nav class="nav">
+		<ul class="nav__list">
+			<li class="nav__list-item">
+				<a href="#">Home</a>
+			</li>
+
+			<li class="nav__list-item  has-children">
+				<a href="#">Find and Watch</a>
+
+				<ul class="nav__list--secondary is-hidden">
+					<li class="go-back"><a href="#0">Menu</a></li>
+					<li class="has-children">
+						<a href="#">Accessories</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">Clothing</a></li>
+							<li class="see-all"><a href="#">All Accessories</a></li>
+							<li><a href="#0">Beanies</a></li>
+							<li><a href="#0">Caps &amp; Hats</a></li>
+							<li><a href="#">Glasses</a></li>
+							<li><a href="#">Gloves</a></li>
+							<li><a href="#">Jewellery</a></li>
+							<li><a href="#">Scarves</a></li>
+							<li><a href="#">Wallets</a></li>
+							<li><a href="#">Watches</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Bottoms</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">Clothing</a></li>
+							<li><a href="#">Casual Trousers</a></li>
+							<li>
+								<a href="#0">Jeans</a>
+							</li>
+							<li><a href="#0">Leggings</a></li>
+							<li><a href="#0">Shorts</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Jackets</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">Clothing</a></li>
+							<li><a href="#">Blazers</a></li>
+							<li><a href="#">Bomber jackets</a></li>
+							<li><a href="#">Denim Jackets</a></li>
+							<li><a href="#">Duffle Coats</a></li>
+							<li><a href="#">Leather Jackets</a></li>
+							<li><a href="#">Parkas</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Tops</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">Clothing</a></li>
+							<li><a href="#">Cardigans</a></li>
+							<li><a href="#">Coats</a></li>
+							<li><a href="#">Hoodies &amp; Sweatshirts</a></li>
+							<li><a href="#">Jumpers</a></li>
+							<li><a href="#">Polo Shirts</a></li>
+							<li><a href="#">Shirts</a></li>
+							<li>
+								<a href="#0">T-Shirts</a>
+							</li>
+							<li><a href="#">Vests</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+		</ul> <!-- primary-nav -->
+	</nav> <!-- cd-nav -->
+
+	<section class="cd-main-content">
+		<!-- your content here -->
+	</section>
+	
+	<script src="/assets/js/build/main.min.js"></script>
+
+</body>
+</html>

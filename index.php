ad12a52dcb5c910f42750047f10f5c7442fd3915
Chris Gorrod <chris@fishtankcreative.co.uk>
2015-09-10 10:19:49 +0100

    Fix break

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, minimum-scale=1.0">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1">
 	<meta property="og:type" content="og:website" />
 	<meta property="og:title" content="SkyWatch" />
 	<meta property="og:image" content="#" />
@@ -29,7 +29,8 @@
 				<a class="logo" href="/">
 					<img src="/assets/img/logo.png" alt="Logo">
 				</a>
-
+			</div>
+			<div class="span12">
 				<ul class="header__buttons">
 					<li>
 						<a class="trigger-button" href="#cd-primary-nav">Menu<span></span></a>
@@ -44,73 +45,117 @@
 	<nav class="nav">
 		<ul class="nav__list">
 			<li class="nav__list-item">
-				<a href="#">Home</a>
+				<a href="#" class="nav__list-link">Home</a>
 			</li>
 
 			<li class="nav__list-item  has-children">
-				<a href="#">Find &amp; Watch TV</a>
+				<a href="#" class="nav__list-link">Find &amp; Watch TV</a>
 
 				<ul class="nav__list--secondary is-hidden">
 					<li class="go-back"><a href="#0">Menu</a></li>
 					<li class="has-children">
-						<a href="#">Accessories</a>
+						<a href="#">Plan your viewing</a>
 
 						<ul class="is-hidden">
-							<li class="go-back"><a href="#0">Clothing</a></li>
-							<li><a href="#">Beanies</a></li>
-							<li><a href="#">Caps &amp; Hats</a></li>
-							<li><a href="#">Glasses</a></li>
-							<li><a href="#">Gloves</a></li>
-							<li><a href="#">Jewellery</a></li>
-							<li><a href="#">Scarves</a></li>
-							<li><a href="#">Wallets</a></li>
-							<li><a href="#">Watches</a></li>
+							<li class="go-back"><a href="#0">Plan your viewing</a></li>
+							<li><a href="#">Full TV listings</a></li>
+							<li><a href="#">HD channel listings</a></li>
+							<li><a href="#">Accessible TV guide</a></li>
+							<li><a href="#">Sky box sets</a></li>
 						</ul>
 					</li>
 
 					<li class="has-children">
-						<a href="#">Bottoms</a>
+						<a href="#">Sky Channels</a>
 
 						<ul class="is-hidden">
-							<li class="go-back"><a href="#0">Clothing</a></li>
-							<li><a href="#">Casual Trousers</a></li>
-							<li><a href="#">Jeans</a></li>
-							<li><a href="#">Leggings</a></li>
-							<li><a href="#">Shorts</a></li>
+							<li class="go-back"><a href="#0">Sky channels</a></li>
+							<li><a href="#">Sky 1</a></li>
+							<li><a href="#">Sky Living</a></li>
+							<li><a href="#">Sky Atlantic</a></li>
+							<li><a href="#">Sky Arts</a></li>
+							<li><a href="#">Sky Movies</a></li>
+							<li><a href="#">Sky Sports</a></li>
+							<li><a href="#">Sky News</a></li>
 						</ul>
 					</li>
 
 					<li class="has-children">
-						<a href="#">Jackets</a>
+						<a href="#">Ways To Watch</a>
 
 						<ul class="is-hidden">
-							<li class="go-back"><a href="#0">Clothing</a></li>
-							<li><a href="#">Blazers</a></li>
-							<li><a href="#">Bomber jackets</a></li>
-							<li><a href="#">Denim Jackets</a></li>
-							<li><a href="#">Duffle Coats</a></li>
-							<li><a href="#">Leather Jackets</a></li>
-							<li><a href="#">Parkas</a></li>
-						</ul>
-					</li>
-
-					<li class="has-children">
-						<a href="#">Tops</a>
-
-						<ul class="is-hidden">
-							<li class="go-back"><a href="#0">Clothing</a></li>
-							<li><a href="#">Cardigans</a></li>
-							<li><a href="#">Coats</a></li>
-							<li><a href="#">Hoodies &amp; Sweatshirts</a></li>
-							<li><a href="#">Jumpers</a></li>
-							<li><a href="#">Polo Shirts</a></li>
-							<li><a href="#">Shirts</a></li>
-							<li><a href="#">T-Shirts</a></li>
-							<li><a href="#">Vests</a></li>
+							<li class="go-back"><a href="#0">Ways To Watch</a></li>
+							<li><a href="#">Remote Record</a></li>
+							<li><a href="#">Never Miss shows you love</a></li>
+							<li><a href="#">Get more from Sky+</a></li>
+							<li><a href="#">Watch with Sky Go</a></li>
 						</ul>
 					</li>
 				</ul>
 			</li>
+
+			<li class="nav__list-item  has-children">
+				<a href="#" class="nav__list-link  active">Shop</a>
+
+				<ul class="nav__list--secondary is-hidden">
+					<li class="go-back"><a href="#0">Menu</a></li>
+					<li class="has-children">
+						<a href="#">TV</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">TV</a></li>
+							<li><a href="#">TV bundles</a></li>
+							<li><a href="#">Channels &amp; shows</a></li>
+							<li><a href="#">Ways to watch</a></li>
+							<li><a href="#">Equipment</a></li>
+							<li><a href="#">Installation &amp; set up</a></li>
+						</ul>
+					</li>
+
+
+					<li class="has-children">
+						<a href="#">Broadband &amp; Talk</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">Broadband &amp; Talk</a></li>
+							<li><a href="#">Compare Broadband</a></li>
+							<li><a href="#">Broadband speeds</a></li>
+							<li><a href="#">Compare Talk</a></li>
+							<li><a href="#">Equipment</a></li>
+							<li><a href="#">Installation &amp; set up</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Wearables</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">Wearables</a></li>
+							<li><a href="/">Sky Watch</a></li>
+						</ul>
+					</li>
+
+					<li class="has-children">
+						<a href="#">Switching to Sky</a>
+
+						<ul class="is-hidden">
+							<li class="go-back"><a href="#0">Switching to Sky</a></li>
+							<li><a href="#">TV</a></li>
+							<li><a href="#">Broadband &amp; Talk</a></li>
+							<li><a href="#">How Sky compares</a></li>
+						</ul>
+					</li>
+				</ul>
+			</li>
+
+			<li class="nav__list-item">
+				<a href="#" class="nav__list-link">My Sky</a>
+			</li>
+
+			<li class="nav__list-item">
+				<a href="#" class="nav__list-link">Help &amp; Support</a>
+			</li>
+
 		</ul> 
 	</nav> <!-- close nav -->
 
@@ -144,7 +189,7 @@
 
 	</section>
 
-	<section class="clearfix"  style="position: relative; height: 1000px;">
+	<section>
 
 		<div class="container">
 
@@ -160,11 +205,40 @@
 				<div class="span6">
 
 					<h1>Lorem ipsum dolor</h1>
-<h2>Lorem ipsum dolor</h2>
-<h3>Lorem ipsum dolor</h3>
-<h4>Lorem ipsum dolor</h4>
-<h5>Lorem ipsum dolor</h5>
-<h6>Lorem ipsum dolor</h6>
+					<h2>Lorem ipsum dolor</h2>
+					<h3>Lorem ipsum dolor</h3>
+					<h4>Lorem ipsum dolor</h4>
+					<h5>Lorem ipsum dolor</h5>
+					<h6>Lorem ipsum dolor</h6>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="full-banner">
+
+		<div class="container">
+
+			<div class="group">
+
+				<div class="span6">
+
+					<p>Testing</p>
+
+				</div>
+
+				<div class="span6">
+
+					<h1>Lorem ipsum dolor</h1>
+					<h2>Lorem ipsum dolor</h2>
+					<h3>Lorem ipsum dolor</h3>
+					<h4>Lorem ipsum dolor</h4>
+					<h5>Lorem ipsum dolor</h5>
+					<h6>Lorem ipsum dolor</h6>
 
 				</div>
 
